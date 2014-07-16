a788cfc3e104c2fc1ad17ad783ce7cac222815e0
luis <lrosario101@gmail.com>
2014-07-16 01:25:05 -0400

    spacing

--- hello.py
+++ hello.py
@@ -198,11 +198,11 @@ def view_profile(user):
 # articles
 @app.route('/articles/')
 def articles_index():
-	return 'all articles'
+		return 'all articles'
 
 @app.route('/articles/<slug>')
 def read_article(slug):
-	return 'article: %s' % slug
+		return 'article: %s' % slug
 
 
 
@@ -210,11 +210,11 @@ def read_article(slug):
 # cups
 @app.route('/cups/')
 def cups_index():
-	return 'all cups in the CONCACAF'
+		return 'all cups in the CONCACAF'
 
 @app.route('/cups/<cup>')
 def show_cup(cup):
-	return 'all about the %s' % cup
+		return 'all about the %s' % cup
 
 
 
@@ -223,12 +223,12 @@ def show_cup(cup):
 @app.route('/leagues/')
 @app.route('/ligas/')
 def leagues_index():
-	return 'all leagues in the CONCACAF'
+		return 'all leagues in the CONCACAF'
 
 @app.route('/leagues/<league>')
 @app.route('/ligas/<league>')
 def show_league(league):
-	return 'all about %s' % league
+		return 'all about %s' % league
 
 
 
@@ -236,11 +236,11 @@ def show_league(league):
 # teams
 @app.route('/teams/')
 def teams_index():
-	return 'all teams in the CONCACAF'
+		return 'all teams in the CONCACAF'
 
 @app.route('/teams/<team>')
 def show_team(team):
-	return 'all about %s' % team
+		return 'all about %s' % team
 
 
 
@@ -248,11 +248,11 @@ def show_team(team):
 # players
 @app.route('/players/')
 def players_index():
-	return 'all players in the CONCACAF'
+		return 'all players in the CONCACAF'
 
 @app.route('/players/<player>')
 def show_player(player):
-	return 'all about %s' % player
+		return 'all about %s' % player
 
 
 
@@ -260,11 +260,11 @@ def show_player(player):
 # managers
 @app.route('/managers/')
 def managers_index():
-	return 'all managers in the CONCACAF'
+		return 'all managers in the CONCACAF'
 
 @app.route('/managers/<manager>')
 def show_manager(manager):
-	return 'all about %s' % manager
+		return 'all about %s' % manager
 
 
 
@@ -272,7 +272,7 @@ def show_manager(manager):
 # settings
 @app.route('/settings/')
 def change_settings():
-	return 'cahnge site settings for user'
+		return 'cahnge site settings for user'
 
 
 
@@ -280,30 +280,30 @@ def change_settings():
 # search
 @app.route('/search/')
 def search_ds():
-	return 'Search'
+		return 'Search'
 
 
 
 # about
 @app.route('/about/')
 def about_ds():
-	return 'Started in edgars room 4 years ago now'
+		return 'Started in edgars room 4 years ago now'
 
 
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'), 404
+		return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-	return render_template('500.html'), 500
+		return render_template('500.html'), 500
 
 
 
 
 
 if __name__ == '__main__':
-	manager.run()
+		manager.run()
