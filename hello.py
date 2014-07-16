65e64c9c06d01661ee004a723872adfa5f8f4628
luis <lrosario101@gmail.com>
2014-07-16 01:23:40 -0400

    using redirects and sessions now for form posts. post/redirect/get

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.moment import Moment
 
@@ -24,143 +24,112 @@ def csrf_error(reason):
 # forms debug page
 @app.route('/forms-test/', methods=['GET', 'POST'])
 def debug_name_form():
-	# pretty much all the info we can store on people..
-	name = None
-	name_form = NameForm()
-	if name_form.validate_on_submit():
-		name = name_form.name.data
-		name_form.name.data = ''
+		# pretty much all the info we can store on people..
+		name_form = NameForm()
+		if name_form.validate_on_submit():
+			session['name'] = name_form.name.data
+			return redirect(url_for('debug_name_form'))
 
-	return render_template('debug/forms-name.html',
-		name_form = name_form,
-		name = name
-		)
+		return render_template('debug/forms-name.html',
+			name_form = name_form,
+			name = session.get('name')
+			)
 
 
 # forms debug page
 @app.route('/forms/', methods=['GET', 'POST'])
 def debug_forms():
-		# pretty much all the info we can store on people..
-		name = None
-		email = None
-		password = None
-		password2 = None
-		remember_me = None
-		username = None
-		agree_to_terms = None
-		text_body = None
-		follow_replies = None
-		search_text = None
-		message = None
-		fullname = None
-		twitter = None
-		instagram = None
-		bio = None
-		picture = None
-
 
 		name_form = NameForm()
 		if name_form.validate_on_submit():
-				name = name_form.name.data
-				name_form.name.data = ''
+				session['name'] = name_form.name.data
+				return redirect(url_for('debug_forms'))
 
 
 		login_form = LoginForm()
 		if login_form.validate_on_submit():
-				email = login_form.email.data
-				login_form.email.data = ''
-				password = login_form.password.data
-				login_form.password.data = ''
-				remember_me = login_form.remember_me.data
-				login_form.remember_me.data = ''
+				session['email'] = login_form.email.data
+				session['password'] = login_form.password.data
+				session['remember_me'] = login_form.remember_me.data
+				return redirect(url_for('debug_forms'))
 
 
 		register_form = RegisterForm()
 		if register_form.validate_on_submit():
-				username = register_form.username.data
-				register_form.username.data = ''
-				email = register_form.email.data
-				register_form.email.data = ''
-				password = register_form.password.data
-				register_form.password.data = ''
-				agree_to_terms = register_form.agree_to_terms.data
-				register_form.agree_to_terms.data = ''
+				session['username'] = register_form.username.data
+				session['email'] = register_form.email.data
+				session['password'] = register_form.password.data
+				session['agree_to_terms'] = register_form.agree_to_terms.data
+				return redirect(url_for('debug_forms'))
 
 
 		changeemail_form = ChangeEmailForm()
 		if changeemail_form.validate_on_submit():
-				email = changeemail_form.email.data
-				changeemail_form.email.data = ''
+				session['email'] = changeemail_form.email.data
+				return redirect(url_for('debug_forms'))
 
 
 		changepassword_form = ChangePasswordForm()
 		if changepassword_form.validate_on_submit():
-				current_password = changepassword_form.current_password.data
-				changepassword_form.current_password.data = ''
-				new_password = changepassword_form.new_password.data
-				changepassword_form.new_password.data = ''
+				session['current_password'] = changepassword_form.current_password.data
+				session['new_password'] = changepassword_form.new_password.data
+				return redirect(url_for('debug_forms'))
 
 
 		resetpassword_form = ResetPasswordForm()
 		if resetpassword_form.validate_on_submit():
-				email = resetpassword_form.email.data
-				resetpassword_form.email.data = ''
+				session['email'] = resetpassword_form.email.data
+				return redirect(url_for('debug_forms'))
 
 
 		comment_form = CommentForm()
 		if comment_form.validate_on_submit():
-				body = comment_form.body.data
-				comment_form.body.data = ''
-				follow_replies = comment_form.follow_replies.data
-				comment_form.follow_replies.data = ''
+				session['body'] = comment_form.body.data
+				session['follow_replies'] = comment_form.follow_replies.data
+				return redirect(url_for('debug_forms'))
 
 
 		follow_form = FollowForm()
 		if follow_form.validate_on_submit():
-				# do something
-				pass
+				# session['#'] do something
+				return redirect(url_for('debug_forms'))
+
 
 		search_form = SearchForm()
 		if search_form.validate_on_submit():
-				text = search_form.text.data
-				search_form.text.data = ''
+				session['text'] = search_form.text.data
+				return redirect(url_for('debug_forms'))
 
 
 		contact_form = ContactForm()
 		if contact_form.validate_on_submit():
-				message = contact_form.message.data
-				contact_form.message.data = ''
-				name = contact_form.name.data
-				contact_form.name.data = ''
-				email = contact_form.email.data
-				contact_form.email.data = ''
+				session['message'] = contact_form.message.data
+				session['name'] = contact_form.name.data
+				session['email'] = contact_form.email.data
+				return redirect(url_for('debug_forms'))
 
 
 
 		profile_form = ProfileForm()
 		if profile_form.validate_on_submit():
-				fullname = profile_form.fullname.data
-				profile_form.fullname.data = ''
-				twitter = profile_form.twitter.data
-				profile_form.twitter.data = ''
-				instagram = profile_form.instagram.data
-				profile_form.instagram.data = ''
-				bio = profile_form.bio.data
-				profile_form.bio.data = ''
-				picture = profile_form.picture.data
-				profile_form.picture.data = ''
+				session['fullname'] = profile_form.fullname.data
+				session['twitter'] = profile_form.twitter.data
+				session['instagram'] = profile_form.instagram.data
+				session['bio'] = profile_form.bio.data
+				session['picture'] = profile_form.picture.data
+				return redirect(url_for('debug_forms'))
 
 
 		bookmark_form = BookmarkForm()
 		if bookmark_form.validate_on_submit():
-				# do something
-				pass
+				# session['#'] do something
+				return redirect(url_for('debug_forms'))
 
 
 		subscribe_form = SubscribeForm()
 		if subscribe_form.validate_on_submit():
-				# do something
-				pass
+				# session['#'] do something
+				return redirect(url_for('debug_forms'))
 
 
 		return render_template('debug/forms.html',
@@ -177,49 +146,49 @@ def debug_forms():
 				profile_form = profile_form,
 				bookmark_form = bookmark_form,
 				subscribe_form = subscribe_form,
-				name = name,
-				email = email,
-				password = password,
-				password2 = password2,
-				remember_me = remember_me,
-				username = username,
-				agree_to_terms = agree_to_terms,
-				text_body = text_body,
-				follow_replies = follow_replies,
-				search_text = search_text,
-				message = message,
-				fullname = fullname,
-				twitter = twitter,
-				instagram = instagram,
-				bio = bio,
-				picture = picture
+				name = session.get('name'),
+				email = session.get('email'),
+				password = session.get('password'),
+				password2 = session.get('password2'),
+				remember_me = session.get('remember_me'),
+				username = session.get('username'),
+				agree_to_terms = session.get('agree_to_terms'),
+				text_body = session.get('text_body'),
+				follow_replies = session.get('follow_replies'),
+				search_text = session.get('search_text'),
+				message = session.get('message'),
+				fullname = session.get('fullname'),
+				twitter = session.get('twitter'),
+				instagram = session.get('instagram'),
+				bio = session.get('bio'),
+				picture = session.get('picture')
 				)
 
 
 @app.route('/')
 def index():
-	form = NameForm()
+		form = NameForm()
 
-	return render_template('ds/00-homepage.html',
-		title = 'DS',
-		description = 'Your source for the CONCACAF',
-		page_id = 'homepage',
-		data_page = 'homepage',
-		current_time = datetime.utcnow(),
-		form = form
-		)
+		return render_template('ds/00-homepage.html',
+				title = 'DS',
+				description = 'Your source for the CONCACAF',
+				page_id = 'homepage',
+				data_page = 'homepage',
+				current_time = datetime.utcnow(),
+				form = form
+				)
 
 
 
 # users
 @app.route('/u/<user>')
 def view_profile(user):
-	return render_template('ds/03-profile.html',
-		title = user,
-		description = 'Profile for user',
-		data_page = 'profile',
-		username = user
-		)
+		return render_template('ds/03-profile.html',
+				title = user,
+				description = 'Profile for user',
+				data_page = 'profile',
+				username = user
+				)
 
 
 
