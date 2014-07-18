5c87dd6dbb81d87fe45813d249d8f7490ff05869
luis <lrosario101@gmail.com>
2014-07-17 23:39:58 -0400

    can send asynchronous email

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager, Shell
@@ -69,14 +70,19 @@ class User(db.Model):
 
 
 
-
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
         sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
 
 
 
