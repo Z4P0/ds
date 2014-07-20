db6fdd2121e814465b8faf432de02110bb1be99a
luis <lrosario101@gmail.com>
2014-07-20 02:26:56 -0400

    added authentication blueprint

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 To run the site:
-$ python helloy.py runserver
+$ python manage.py runserver
+
+Dev setup:
+export MAIL_USERNAME=yourusername@gmail.com
+export MAIL_PASSWORD=password
+export DS_ADMIN=other@gmail.com
+# optional
+export SECRET_KEY='secret_keyLOLOL'
+export FLASK_CONFIG='development'
+
 
 python hello.py runserver --host 0.0.0.0
--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Role(db.Model):
     """ Users can be admins, guests-posts, or regular """
@@ -15,6 +16,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
     fullname = db.Column(db.String(128), nullable=True)
     twitter = db.Column(db.String(128), unique=True, nullable=True)
     instagram = db.Column(db.String(128), unique=True, nullable=True)
@@ -27,6 +29,16 @@ class User(db.Model):
     # comments
     # following
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.username
